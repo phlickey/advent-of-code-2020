18c4c47f54ed4569d24975578083b0b53ed77a51
Philip Hickey <phlickey@gmail.com>
2020-12-09 22:46:37 +0000

    Day 05: Slightly janky way to finish

--- 05/parse.js
+++ 05/parse.js
@@ -4,13 +4,27 @@ const text = fs.readFileSync("input.txt", { encoding: "utf8" });
 
 const tickets = text.split(/\n/);
 
-let max = 0;
-
-for (const ticket of tickets) {
-  if (ticket.length > 0) {
-    const { id } = parseTicket(ticket);
-
-    max = Math.max(max, id);
+const getMaxId = (tickets) => {
+  let max = 0;
+  for (const ticket of tickets) {
+    if (ticket.length > 0) {
+      const { id } = parseTicket(ticket);
+      max = Math.max(max, id);
+    }
   }
-}
-console.log(max);
+  return max;
+};
+
+// console.log(getMaxId(tickets));
+
+const findMySeat = (tickets) => {
+  const allIds = new Set(tickets.map((rawTicket) => parseTicket(rawTicket).id));
+  const seatsWithMissingNeighbours = new Set();
+  for (const id of allIds.values()) {
+    if (!allIds.has(id + 1) || !allIds.has(id - 1)) {
+      seatsWithMissingNeighbours.add(id);
+    }
+  }
+};
+
+findMySeat(tickets);
